2c8bbd4e1c92949482c0648f395b546a48a42c97
Aashish Gaikwad <ashish.gaikwad@verloop.io>
2022-06-24 18:26:29 +0530

    Showing popover on left click

--- src/Block.tsx
+++ src/Block.tsx
@@ -1,14 +1,25 @@
-import { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Handle, Position, useUpdateNodeInternals } from "react-flow-renderer";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 // import { v4 as uuidv4 } from "uuid";
 import "./customNode.css";
-import PopupState, { bindContextMenu, bindMenu } from "material-ui-popup-state";
+import PopupState, {
+  bindContextMenu,
+  bindMenu,
+  bindPopover,
+  bindTrigger,
+} from "material-ui-popup-state";
 import { PopupState as PState } from "material-ui-popup-state/core";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "./store";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 
 interface Item {
   id: string;
@@ -18,7 +29,35 @@ interface Item {
 function BlockNode({ id, data }: any) {
   const updateNodeInternals = useUpdateNodeInternals();
   const dispatch = useDispatch();
-  const nodes = useSelector((state: AppState) => state.nodes);
+
+  const onActionButtonClick = (popState: PState) => {
+    popState.close();
+  };
+
+  const card = (popState: PState) => {
+    return (
+      <React.Fragment>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Header
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, mb: 1.5 }}
+            color="text.secondary"
+            gutterBottom
+          >
+            Subheader
+          </Typography>
+          <Typography variant="body2">content goes here...</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => onActionButtonClick(popState)}>
+            Action Button
+          </Button>
+        </CardActions>
+      </React.Fragment>
+    );
+  };
 
   const onDelete = useCallback(() => {
     dispatch({ type: "DELETE_NODE" });
@@ -118,65 +157,100 @@ function BlockNode({ id, data }: any) {
                                 >
                                   {(popupStateChild) => (
                                     <div onContextMenu={onContextClickChild}>
-                                      <div
-                                        {...provided.draggableProps}
-                                        ref={provided.innerRef}
-                                        className="nodrag"
-                                        style={{
-                                          ...provided.draggableProps.style,
-                                          top: "auto !important",
-                                          left: "auto !important",
-                                          width: "100%",
-                                          height: "100%",
-                                          cursor: "pointer",
-                                          // background: snapshot.isDragging
-                                          //   ? "#000"
-                                          //   : "#fff",
-                                          //transform,
-                                        }}
-                                        id={`id1${index}`}
-                                        key={`id1${index}`}
-                                        {...bindContextMenu(popupStateChild)}
+                                      <PopupState
+                                        variant="popover"
+                                        popupId="demo-popup-popover"
                                       >
-                                        <div
-                                          style={{
-                                            position: "relative",
-                                          }}
-                                          id={`id2${index}`}
-                                          key={`id2${index}`}
-                                        >
-                                          <label
-                                            htmlFor="text"
-                                            style={{ margin: "8px" }}
-                                            {...provided.dragHandleProps}
-                                          >
-                                            {kid.content}
-                                          </label>
-                                          <Handle
-                                            type="source"
-                                            position={Position.Right}
-                                            id={kid.id}
-                                            key={kid.id}
-                                          />
-                                        </div>
-                                      </div>
-                                      <Menu {...bindMenu(popupStateChild)}>
-                                        <MenuItem
-                                          onClick={() =>
-                                            onDuplicateChild(
-                                              popupStateChild,
-                                              kid.id
-                                            )
-                                          }
-                                        >
-                                          Duplicate
-                                        </MenuItem>
-                                        <MenuItem
-                                          onClick={() => onDeleteChild(kid.id)}
-                                        >
-                                          Delete
-                                        </MenuItem>
-                                      </Menu>
+                                        {(popupStatePop) => (
+                                          <>
+                                            <div
+                                              {...provided.draggableProps}
+                                              ref={provided.innerRef}
+                                              className="nodrag"
+                                              style={{
+                                                ...provided.draggableProps
+                                                  .style,
+                                                top: "auto !important",
+                                                left: "auto !important",
+                                                width: "100%",
+                                                height: "100%",
+                                                cursor: "pointer",
+                                                // background: snapshot.isDragging
+                                                //   ? "#000"
+                                                //   : "#fff",
+                                                //transform,
+                                              }}
+                                              id={`id1${index}`}
+                                              key={`id1${index}`}
+                                              {...bindContextMenu(
+                                                popupStateChild
+                                              )}
+                                              {...bindTrigger(popupStatePop)}
+                                            >
+                                              <div
+                                                style={{
+                                                  position: "relative",
+                                                }}
+                                                id={`id2${index}`}
+                                                key={`id2${index}`}
+                                              >
+                                                <label
+                                                  htmlFor="text"
+                                                  style={{ margin: "8px" }}
+                                                  {...provided.dragHandleProps}
+                                                >
+                                                  {kid.content}
+                                                </label>
+                                                <Handle
+                                                  type="source"
+                                                  position={Position.Right}
+                                                  id={kid.id}
+                                                  key={kid.id}
+                                                />
+                                              </div>
+                                            </div>
+                                            <Menu
+                                              {...bindMenu(popupStateChild)}
+                                            >
+                                              <MenuItem
+                                                onClick={() =>
+                                                  onDuplicateChild(
+                                                    popupStateChild,
+                                                    kid.id
+                                                  )
+                                                }
+                                              >
+                                                Duplicate
+                                              </MenuItem>
+                                              <MenuItem
+                                                onClick={() =>
+                                                  onDeleteChild(kid.id)
+                                                }
+                                              >
+                                                Delete
+                                              </MenuItem>
+                                            </Menu>
+                                            <Popover
+                                              {...bindPopover(popupStatePop)}
+                                              anchorOrigin={{
+                                                vertical: "bottom",
+                                                horizontal: "left",
+                                              }}
+                                              transformOrigin={{
+                                                vertical: "top",
+                                                horizontal: "center",
+                                              }}
+                                            >
+                                              <Card
+                                                variant="outlined"
+                                                style={{ margin: "0px" }}
+                                              >
+                                                {card(popupStatePop)}
+                                              </Card>
+                                            </Popover>
+                                          </>
+                                        )}
+                                      </PopupState>
                                     </div>
                                   )}
                                 </PopupState>
